aec2aa2739bb4e34c0d762eeb132c1089629a3bc
akitoshi-nt <akitoshi.nt@users.noreply.github.com>
2021-03-10 23:18:55 +0900

    xbee

--- Keeper/Xbee/Xbee.ino
+++ Keeper/Xbee/Xbee.ino
@@ -1,25 +1,33 @@
+/*
+   ロボット側(SofrwareSerial)から来た
+   データをそのままxbee(HardwareSerial)
+   に送り出す。
+   23.nov.2020 by H.Saeki
+*/
+
 #include <SoftwareSerial.h>
-SoftwareSerial xbeeSerial(10, 11); //メインCPUに繋がるシリアルポート
+SoftwareSerial xbeeSerial(10, 11); // メインCPUに繋がるシリアルポート
 
 int led = 4;
-void setup(){
-    Serial.begin(9600); //Xbee向けのシリアルポートを初期化
-    xbeeSerial.begin(9600); //メインCPU向けのシリアルポートを初期化
+void setup() {
+  Serial.begin(9600); // Xbee向けのシリアルポートを初期化
+  xbeeSerial.begin(9600); //　メインCPU向けのシリアルポートを初期化
+  
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  Serial.println(" Ready!");  // Xbeeに文字を出力
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);
+  digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
 
-    // initalize the digital pin as an output.
-    pinMode(led, OUTPUT);
-    Serial.println(" Ready!"); //Xbeeに文字出力
-    digitalWrite(led, HIGH); //turn the LED on
-    delay(100);
-    digitalWrite(led, LOW); //turn the LED off
 }
 
-void loop(){
-    char buf[4];
-    xbeeSerial.listen();
-    if(xbeeSerial.available() > 0){
-        digitalWrite(led, HIGH);
-        Serial.write(xbeeSerial.read()); //メインCPUから来たデータをXbeeに送り出す
-        digitalWrite(led, LOW);
-    }
+void loop() {
+  char buf[4];
+  xbeeSerial.listen();  // メインCPUからのデータを受信する
+  if (xbeeSerial.available() > 0) {
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.write(xbeeSerial.read());  // メインCPUから来たデータをXbeeに送り出す。
+    digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
+  }
 }
