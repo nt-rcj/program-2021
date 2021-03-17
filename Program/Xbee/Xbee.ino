5d2c589db6f1cc59f41ab2004275797d4fcd7748
akitoshi-nt <akitoshi.nt@users.noreply.github.com>
2021-03-17 15:53:42 +0900

    353

--- Program/Xbee/Xbee.ino
+++ Program/Xbee/Xbee.ino
@@ -9,40 +9,40 @@
 SoftwareSerial xbeeSerial(10, 11); // メインCPUに繋がるシリアルポート
 
 int led = 4;
-int me = 0;
-int you = 0;
+uint8_t me = 0;
+uint8_t you = 0;
 void setup() {
   Serial.begin(9600); // Xbee向けのシリアルポートを初期化
   xbeeSerial.begin(9600); //　メインCPU向けのシリアルポートを初期化
-  
+
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
-  Serial.println(" Ready!");  // Xbeeに文字を出力
+  //Serial.println(" Ready!");  // Xbeeに文字を出力
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);
   digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
+  xbeeSerial.listen();  // メインCPUからのデータを受信する
 
 }
 
 void loop() {
-  char buf[4];
-  xbeeSerial.listen();  // メインCPUからのデータを受信する
   if (xbeeSerial.available() > 0) {
     me =  xbeeSerial.read();
     Serial.write(me);  // メインCPUから来たデータをXbeeに送り出す。
   }
-  if (Serial.available() > 0){
+  if (Serial.available() > 0) {
     you = Serial.read();
-  if (me <= you){
-    //attacker
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-    xbeeSerial.print("1");
-    digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)     
-  }else{
-    //keeper
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-    xbeeSerial.print("2");
-    digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)    
-  }
+    if (me <= you) {
+      //attacker
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+      xbeeSerial.print("1");
+      digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
+    } else {
+      //keeper
+      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+      xbeeSerial.print("2");
+      digitalWrite(led, LOW);   // turn the LED off (LOW is the voltage level)
+    }
+    delay(20);
   }
 }
